3fd86d2d9638e0a6905c4ba8963d8ea487618c44
Polobear9 <lieams1996@gmail.com>
2022-09-10 12:02:03 +0900

    Spring Test study

---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTest {
+
+    @Test
+    void home() {
+    }
+
+    @Test
+    void main() {
+    }
+}
